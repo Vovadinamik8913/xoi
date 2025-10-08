c02afc66936742e17aa32ae96304dfd0b1e6baa0
v.m.abramenko <v.m.abramenko@tbank.ru>
2025-10-08 09:17:25 +0700

    additional info

--- spider/src/main/java/ru/nsu/abramenko/Main.java
+++ spider/src/main/java/ru/nsu/abramenko/Main.java
@@ -10,10 +10,14 @@ public class Main {
     public static void main(String[] args) {
         var messages = Collections.synchronizedList(new ArrayList<String>());
         var spider = new Spider("localhost", 8080);
+        var time = System.nanoTime();
         var init = Thread.ofVirtual()
                 .start( new Find("/", messages, spider));
         init.join();
+        var end = System.nanoTime();
+        System.out.println("Time: " + (end - time) / 1_000_000_000 + " seconds");
         messages.sort(String::compareTo);
+        System.out.println("Found " + messages.size() + " messages:");
         for (var message : messages) {
             System.out.println(message);
         }
